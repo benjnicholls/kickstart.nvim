1712ccc949ca49c8ce97fc0ab8120e4eb9cc4631
Ben Nicholls <ben@thetica.com>
2026-01-27 09:14:15 -0500

    Remove unneeded plugins. Create new autocommand to format tabs on sql files

--- lua/custom/plugins/remote-sshfs.lua
+++
@@ -1,9 +0,0 @@
--- Using lazy.nvim
-return {
-  'nosduco/remote-sshfs.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-  opts = {
-    -- Refer to the configuration section below
-    -- or leave empty for defaults
-  },
-}
