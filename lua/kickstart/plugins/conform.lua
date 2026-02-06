35d37dc2e79d1670fa10a9d2a00de4fd5ef1bc88
Ben Nicholls <benjnicholls@pm.me>
2026-02-06 14:47:09 -0500

    Modify options so that biome formats javascript code and also can do relative jumps.

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -32,6 +32,7 @@ return { -- Autoformat
       lua = { 'stylua' },
       python = { 'ruff' },
       sql = { 'sqlfluff' },
+      javascript = { 'biome' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
